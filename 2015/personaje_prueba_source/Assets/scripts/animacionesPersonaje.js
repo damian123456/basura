5edf861cf8266ca6eb636761397dc563e8277e6c
Fede Crespo <fedecrespo90@gmail.com>
2015-01-16 05:13:44 -0200

    Proyecto unity: personaje camina y salta. NO CARGAN SPRITES

---
+++ 2015/personaje_prueba_source/Assets/scripts/animacionesPersonaje.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+	var imagen : Texture;
+	var imagen1 : Texture;
+	
+	if(Input.GetKeyDown("1"))
+		renderer.material.SetTexture("_BumpMap", imagen);
+	if(Input.GetKeyDown("2"))
+		renderer.material.SetTexture("_BumpMap", imagen1);
