7b62d81cdad1a8935f698702e137f0f4cabea19c
Fede Crespo <fedecrespo90@gmail.com>
2015-01-17 16:24:56 -0200

    Cargo los archivos unity para animacion

--- 2015/personaje_prueba_source/Assets/scripts/animacionesPersonaje.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-
-	var imagen : Texture;
-	var imagen1 : Texture;
-	
-	if(Input.GetKeyDown("1"))
-		renderer.material.SetTexture("_BumpMap", imagen);
-	if(Input.GetKeyDown("2"))
-		renderer.material.SetTexture("_BumpMap", imagen1);
