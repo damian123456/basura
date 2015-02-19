5daec0598d15aaf69584491318a9c2908773c1b1
fedecrespo90 <fedecrespo90@gmail.com>
2015-02-19 11:46:01 -0300

    Upload slider-empresas.php

--- ei/slider-empresas.php
+++ ei/slider-empresas.php
@@ -4,9 +4,9 @@
         <a href="#">
             <div class="container-caption-empresas">
                 <div class="caption-empresas">
-                    <p><a href="empresas.php">Cursos para empresas</a></p>
-                    <p><a href="traducciones.php">Traducciones</a></p>
-                    <p><a href="interpretes.php">Interpretes</a></p>
+                    <p><a href="content.php?content=69&category=120">Cursos para empresas</a></p>
+                    <p><a href="content.php?content=77&category=120">Traducciones</a></p>
+                    <p><a href="content.php?content=78&category=120">Interpretes</a></p>
 					<p class="last"><a href="contacto.php">Contacto</a></p>
                 </div>
             </div>
@@ -17,9 +17,9 @@
         <a href="#">
             <div class="container-caption-empresas">
                 <div class="caption-empresas">
-                   <p><a href="empresas.php">Cursos para empresas</a></p>
-                    <p><a href="traducciones.php">Traducciones</a></p>
-                    <p><a href="interpretes.php">Interpretes</a></p>
+                   <p><a href="content.php?content=69&category=120">Cursos para empresas</a></p>
+                    <p><a href="content.php?content=77&category=120">Traducciones</a></p>
+                    <p><a href="content.php?content=78&category=120">Interpretes</a></p>
 					<p class="last"><a href="contacto.php">Contacto</a></p>
                 </div>
             </div>
@@ -30,9 +30,9 @@
         <a href="#">
             <div class="container-caption-empresas">
                 <div class="caption-empresas">
-                    <p><a href="empresas.php">Cursos para empresas</a></p>
-                    <p><a href="traducciones.php">Traducciones</a></p>
-                    <p><a href="interpretes.php">Interpretes</a></p>
+                    <p><a href="content.php?content=69&category=120">Cursos para empresas</a></p>
+                    <p><a href="content.php?content=77&category=120">Traducciones</a></p>
+                    <p><a href="content.php?content=78&category=120">Interpretes</a></p>
 					<p class="last"><a href="contacto.php">Contacto</a></p>
                 </div>
             </div>
